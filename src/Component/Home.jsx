8f56a1a08d9484a08d73149453a6a59d212013eb
sachin kumar <kumarsachin88131@gmail.com>
2025-08-21 12:46:40 +0530

    commit dflobnhjk

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -31,8 +31,8 @@ const Home = () => {
          {
                             users.map((user) => {
                                 return (
-                                    <div className="col-12 col-md-3 dicnun" >
-                                        <Link className='Link' to={{ pathname: "/hometwo", search: `id=${user._id}` }} >
+                                    <div className="col-12 col-md-3 dicnun underline" >
+                                        <Link  to={{ pathname: "/hometwo", search: `id=${user._id}` }} >
                                             <div class="card mb-3" key={user._id}>
                                                 {
                                                     user.files.map((file) => {
