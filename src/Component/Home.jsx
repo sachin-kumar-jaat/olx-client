600ff02dbdb9d372b193481c258ea733f06bdf5a
sachin kumar <kumarsachin88131@gmail.com>
2025-08-21 11:36:53 +0530

    commit dflobnh

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -33,7 +33,7 @@ const Home = () => {
                                 return (
                                     <div className="col-12 col-md-3 dicnun" >
                                         <Link to={{ pathname: "/hometwo", search: `id=${user._id}` }} >
-                                            <div class="card mb-3 Link" key={user._id}>
+                                            <div class="card mb-3" key={user._id}>
                                                 {
                                                     user.files.map((file) => {
                                                         return (
@@ -41,7 +41,7 @@ const Home = () => {
                                                         )
                                                     })
                                                 }
-                                                <div class="card-body">
+                                                <div class="card-body Link">
                                                     <h2 class="card-title">₹ {user.Price}</h2>
                                                     <p class="card-title"> {user.bhkname} BHK {user.bathroomname} Bathrooms {user.sqftname} Sqft</p>
                                                     <p class="card-title">{user.State}</p>
